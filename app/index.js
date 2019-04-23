1dfb25a32fe57ba79e5d87bb00dd0c94e4a3af2a
sabler <rob_sable@gap.com>
2019-04-23 16:28:27 -0700

    Quick comments

--- app/index.js
+++ app/index.js
@@ -5,20 +5,26 @@ const mdbc = require('./modules/mdbc.js')
 // RestifyJS
 const restify = require('restify');
 
+
+// New instance of the server
 const codeServ = restify.createServer( {
     name: 'FriendsFamily',
     version:'1.0'
 });
 
 
+// Function for initial testing, remove before deployment
 function baseResponse(req, res, next) {
     res.send(`I'm here, employee ${req.params.empId}`);
     next();
 }
 
+
+// Get end point syntax
 codeServ.get('/friendsfamily/jj/:empId', baseResponse);
 
 
+// Frasier Reference
 codeServ.listen(8000,function() {
     console.log("Go ahead, I'm listening...");
 })
