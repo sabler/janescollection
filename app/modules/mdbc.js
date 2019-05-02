b5c052ad26c64c265109d8a2d0d76d65af3eaecc
sabler <rob_sable@gap.com>
2019-05-02 13:16:33 -0700

    Mongo driver's heartbeat test works, safe to proceed

--- app/modules/mdbc.js
+++ app/modules/mdbc.js
@@ -1,2 +1,24 @@
 
-console.log("Pull starting the MongoDB connector...");
+console.log("Attempting to start the MongoDB connector...");
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:32772';
+
+// Database Name
+const dbName = 'myproject';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
